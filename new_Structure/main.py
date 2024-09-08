c25127c54ca75db8e5e574361d9625c04a9314a9
John Cheng <chengpu@stu.scu.edu.cn>
2024-09-08 15:30:47 +0800

    testing

--- new_Structure/main.py
+++ new_Structure/main.py
@@ -204,7 +204,7 @@ def args_config(dataset_choice : int) -> Namespace:
 def main() -> None:
     # REMIND: model must have its name attribute
     args = args_config(
-        dataset_choice=4,
+        dataset_choice=1,
     )
     model = TSMixer(sensors=14, e_layers=8, d_model=36, seq_len=args.windows_size, pred_len=1, dropout=0.2)
     # model = parallel_TSMixer(sensors=14, e_layers=16, d_model=36, seq_len=args.windows_size, pred_len=1, dropout=0.2)
