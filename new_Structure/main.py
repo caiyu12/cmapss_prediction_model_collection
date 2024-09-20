87743aa893ebac31426e3c1e202bf4557c4291ed
cai <1@1>
2024-09-20 16:56:00 +0800

    test

--- new_Structure/main.py
+++ new_Structure/main.py
@@ -192,8 +192,8 @@ def args_config(dataset_choice : int) -> Namespace:
     arguments = Namespace(
         directory = './',
         dataset   = 'FD00{}'.format(dataset_choice),
-        epoch     = 10  ,
-        device    = torch.device("cuda:1" if torch.cuda.is_available() else "cpu"),
+        epoch     = 15,
+        device    = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         max_rul   = 125,
         learning_rate = 0.001,
 
@@ -209,6 +209,7 @@ def args_config(dataset_choice : int) -> Namespace:
         case 2:
             arguments.accept_window = 50
             arguments.window_size_tuple = (arguments.accept_window, 60, 70, 80, 90, 100, 110, 120,)
+            # arguments.window_size_tuple = (arguments.accept_window, 60, 70, 80, 90, 100, 110,)
             arguments.batch_size    = 100
 
         case 3:
@@ -217,9 +218,9 @@ def args_config(dataset_choice : int) -> Namespace:
             arguments.batch_size    = 100
 
         case 4:
-            arguments.accept_window = 40
+            arguments.accept_window = 50
             # arguments.window_size_tuple = (arguments.accept_window, 60, 70, 80, 90, 100, 110, 120,)
-            arguments.window_size_tuple = (arguments.accept_window, 50, 60, 70, 80, )
+            arguments.window_size_tuple = (arguments.accept_window, 60, 70, 80)
             arguments.batch_size    = 100
 
         case _:
@@ -227,10 +228,10 @@ def args_config(dataset_choice : int) -> Namespace:
 
     return arguments
 
-def main(choice) -> None:
+def main() -> None:
     # REMIND: model must have its name attribute
     args = args_config(
-        dataset_choice=choice,
+        dataset_choice=2,
     )
     # model = TSMixer(sensors=14, e_layers=8, d_model=36, seq_len=args.accept_window, pred_len=1, dropout=0.2)
     # model = parallel_TSMixer(sensors=14, e_layers=16, d_model=36, seq_len=args.accept_window, pred_len=1, dropout=0.2)
@@ -242,10 +243,18 @@ def main(choice) -> None:
     # )
 
     model = LSTM_pTSMixer_GA(
-        sensors=14, e_layers=8,
-        t_model=36, c_model=36,
-        lstm_layer_num=8,
-        seq_len=args.accept_window, dropout=0.2, accept_window=args.accept_window)
+        sensors=14, e_layers=16,
+        t_model=48, c_model=36,
+        lstm_layer_num=2,
+        seq_len=args.accept_window, dropout=0.2, accept_window=args.accept_window
+    )
+
+    # model = AP_pTSMixer_GA(
+    #     sensors=14, e_layers=16,
+    #     t_model=48, c_model=36,
+    #     lstm_layer_num=2,
+    #     seq_len=args.accept_window, dropout=0.2, accept_window=args.accept_window
+    # )
 
     # model = ENCODER_LSTM_TSMixer(sensors=14, e_layers=8, d_model=36, seq_len=args.accept_window, pred_len=1, dropout=0.2, accept_window=args.accept_window)
     args.model_name = model.name
@@ -254,6 +263,4 @@ def main(choice) -> None:
     rmse = train.Train_Test()
 
 if __name__ == '__main__':
-    for choice in range(1, 5):
-        for i in range(15):
-            main(choice)
+    main()
